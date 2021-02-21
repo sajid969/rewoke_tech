dfd394a4a9865f8bf07d8003a7a739439a631036
sajid969 <69970953+sajid969@users.noreply.github.com>
2021-02-21 18:19:18 +0530

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -7,5 +7,7 @@ class signupform(forms.ModelForm):
         model=User
         fields=('username','password','email','first_name','last_name')
 
-class filefieldForm(forms.Form):
-    choosefile=forms.FileField()
+class filefieldForm(forms.ModelForm):
+    class Meta:
+        model=filefield
+        fields=['choosefile']
